eab441c0a7208a94893e01a292b67aa307237b1d
Danny <danny.alvarado.monge@est.una.ac.cr>
2026-05-18 12:05:17 -0600

    base del proyecto vite

---
+++ server/src/config/socket.js
@@ -0,0 +1,40 @@
+import { Server } from "socket.io";
+
+export const initSocket = (server) => {
+
+  const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+
+    console.log("Usuario conectado:", socket.id);
+
+    // Unirse a sala
+    socket.on("join_room", (room) => {
+
+      socket.join(room);
+
+      console.log(`${socket.id} se unió a ${room}`);
+
+    });
+
+    // Movimiento
+    socket.on("move_piece", (data) => {
+
+      socket.to(data.room).emit("receive_move", data);
+
+    });
+
+    socket.on("disconnect", () => {
+
+      console.log("Usuario desconectado:", socket.id);
+
+    });
+
+  });
+
+};
