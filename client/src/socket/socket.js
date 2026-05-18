e09684490ba15b29915d4d5e34338d765f6bae4a
Danny <danny.alvarado.monge@est.una.ac.cr>
2026-05-18 12:16:29 -0600

    cambiando configuracion para conectar dos pc

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000");
+export const socket = io("http://192.168.40.218:3000");
--- server/src/config/socket.js
+++ server/src/config/socket.js
@@ -4,7 +4,7 @@ export const initSocket = (server) => {
 
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "*",
       methods: ["GET", "POST"],
     },
   });
